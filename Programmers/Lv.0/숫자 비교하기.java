e53420bc260af9d66adb17d1e26dc7c027cfd3ba
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-06 00:16:16 +0900

    Create 숫자 비교하기.java

---
+++ Programmers/Lv.0/숫자 비교하기.java
@@ -0,0 +1,30 @@
+/**
+ * fileName : 숫자 비교하기.java
+ * author :  한찬영
+ * date : 2023-03-06
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-05            한찬영             최초 생성
+ */
+
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        
+        if (num1==num2) {
+            
+            answer = 1;
+        }
+        else {
+            answer = -1;
+        }
+        
+        
+        return answer;
+        
+    }
+}
