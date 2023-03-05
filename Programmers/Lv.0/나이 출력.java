171438beb14f8ec71cf07d50af35ae1a2273db78
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-06 00:17:07 +0900

    Create 나이 출력.java

---
+++ Programmers/Lv.0/나이 출력.java
@@ -0,0 +1,20 @@
+/**
+ * fileName : 두 수의 곱.java
+ * author :  한찬영
+ * date : 2023-03-06
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-06            한찬영             최초 생성
+ */
+
+
+class Solution {
+    public int solution(int age) {
+        int answers = 0;
+        
+        answers = (2022-age)+1;
+        return answers;
+    }
+}
