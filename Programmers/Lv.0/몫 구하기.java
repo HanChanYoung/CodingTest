afa5349b7ce7c9e6996c2c35691f6c7bec8b9cd2
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-06 00:14:37 +0900

    Create 몫 구하기.java

---
+++ Programmers/Lv.0/몫 구하기.java
@@ -0,0 +1,19 @@
+/**
+ * fileName : 몫 구하기.java
+ * author :  한찬영
+ * date : 2023-03-05
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-05            한찬영             최초 생성
+ */
+
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1/num2;
+        return answer;
+    }
+}
