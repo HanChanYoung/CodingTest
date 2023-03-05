9c919e3a1da3818a418e26fab1d6d87ecca2eb61
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-06 00:18:59 +0900

    Create 두 수의 합.java

---
+++ Programmers/Lv.0/두 수의 합.java
@@ -0,0 +1,18 @@
+/**
+ * fileName : 두 수의 합.java
+ * author :  한찬영
+ * date : 2023-03-06
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-06            한찬영             최초 생성
+ */
+
+
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        return num1+num2;
+    }
+}
