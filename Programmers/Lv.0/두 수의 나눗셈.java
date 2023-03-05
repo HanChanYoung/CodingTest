23d1f636c8bb18ef15fe83cd52dc9c5dda31e7cf
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-06 00:20:53 +0900

    Create 두 수의 나눗셈.java

---
+++ Programmers/Lv.0/두 수의 나눗셈.java
@@ -0,0 +1,21 @@
+/**
+ * fileName : 두 수의 나눗셈.java
+ * author :  한찬영
+ * date : 2023-03-06
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-06            한찬영             최초 생성
+ */
+
+class Solution {
+    public double solution(int num1, int num2) {
+    
+        double answer =  (double)num1/num2*1000;
+        
+        
+        
+        return (int)answer;
+    }
+}
