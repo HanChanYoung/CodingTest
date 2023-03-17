9a1cac4a1c18a46a9c3299e8d2432f0eb795a8a1
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-03-17 16:25:26 +0900

    Create 편지.java

---
+++ Programmers/Lv.0/편지.java
@@ -0,0 +1,20 @@
+/**
+ * fileName : 편지.java
+ * author :  한찬영
+ * date : 2023-03-17
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-03-17            한찬영             최초 생성
+ */
+
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        
+        answer = message.length()*2;
+        
+        return answer;
+    }
+}
