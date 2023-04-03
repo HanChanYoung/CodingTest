5a64056239fd983dcefb0f911dc04b7fa205ad80
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-04-03 15:37:25 +0900

    Create 특이한 정렬.java

---
+++ Programmers/Lv.0/특이한 정렬.java
@@ -0,0 +1,40 @@
+/**
+ * fileName : 특이한 정렬.java
+ * author :  한찬영
+ * date : 2023-04-03
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-04-03            한찬영             최초 생성
+ */
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+class Solution {
+    public Integer[] solution(int[] numlist, int n) {
+        List<Integer> solution = IntStream.of(numlist).boxed().collect(Collectors.toList());
+
+        solution.sort(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int o1Abs = Math.abs(o1 - n);
+                int o2Abs = Math.abs(o2 - n);
+
+                if (o1Abs == o2Abs) {
+                    if (o1 > o2) return -1;
+                    else if (o1 < o2) return 1;
+                } else {
+                    return o1Abs - o2Abs;
+                }
+
+                return 0;
+            }
+        });
+
+        return solution.toArray(new Integer[0]);
+    }
+}
