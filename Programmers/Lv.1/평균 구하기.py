a8a95005c53eeda799ed8a3bacfe550e7a865f43
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-04-03 19:59:03 +0900

    Create 평균 구하기.py

---
+++ Programmers/Lv.1/평균 구하기.py
@@ -0,0 +1,20 @@
+/**
+ * fileName : 평균 구하기.py
+ * author :  한찬영
+ * date : 2023-04-03
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-04-03            한찬영             최초 생성
+ */
+
+def solution(num):
+   
+    if(num%2==0):
+        answer = 'Even'
+
+    else:
+        answer = 'Odd'
+    
+    return answer
