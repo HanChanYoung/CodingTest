4144a3f71a808451b6a86b5b255623f67405f631
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-04-03 20:00:44 +0900

    Create 평균 구하기.py

---
+++ Programmers/Lv.1/평균 구하기.py
@@ -0,0 +1,22 @@
+/**
+ * fileName : 평균 구하기.py
+ * author :  한찬영
+ * date : 2023-04-03
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-04-03            한찬영             최초 생성
+ */
+
+def solution(arr):
+    answer = 0
+    
+    result = sum(arr)
+    lens = len(arr)
+    
+    answer = result/lens
+    
+    
+    
+    return answer
