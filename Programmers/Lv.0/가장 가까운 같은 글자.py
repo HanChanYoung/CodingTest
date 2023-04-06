2125c7fc6e9c2f35751452bcc1a9d5af7c0515ea
Asher <84032164+HanChanYoung@users.noreply.github.com>
2023-04-06 17:30:38 +0900

    Create 가장 가까운 같은 글자.py

---
+++ Programmers/Lv.0/가장 가까운 같은 글자.py
@@ -0,0 +1,22 @@
+/**
+ * fileName : 가장 가까운 같은 글자.py
+ * author :  한찬영
+ * date : 2023-04-06
+ * description :
+ * ===========================================================
+ * DATE                 AUTHOR                NOTE
+ * -----------------------------------------------------------
+ * 2023-04-06            한찬영             최초 생성
+ */
+
+def solution(s):
+    answer = []
+    dic = {}
+    
+    for i in range(len(s)):
+        if s[i] not in dic:
+            answer.append(-1)
+        else:
+            answer.append(i - dic[s[i]])
+        dic[s[i]] = i
+    return answer
